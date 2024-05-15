92fd7c118f2c15353e8b8292fe4cb8a79a910667
Lee <43488305+poiu694@users.noreply.github.com>
2024-05-15 16:27:38 +0900

    feat: inject queryClient to root with ReactQueryStreamedHydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
 import './globals.css';
+import Providers from './provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
