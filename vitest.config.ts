a67136adde6c43e7c6084432ea9acab6e4ec8d39
hee-suh <suhee0206@gmail.com>
2024-05-16 15:28:35 +0900

    feat: add vitest for nextjs
    - https://vitest.dev/guide/
    - https://nextjs.org/docs/app/building-your-application/testing/vitest

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    exclude: ["**/node_modules/**", "**/dist/**"],
+    coverage: {
+      include: ["src/**/*.ts"],
+      provider: "v8",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
