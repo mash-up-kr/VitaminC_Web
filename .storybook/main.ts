97d0306c95ef96a90669913c2bb19cf5b81f985b
hee-suh <suhee0206@gmail.com>
2024-05-17 21:12:51 +0900

    feat: add SVGR support to Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -23,6 +23,24 @@ const config: StorybookConfig = {
       };
     }
 
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+
+    // This modifies the existing image rule to exclude .svg files
+    // since you want to handle those files with @svgr/webpack
+    const imageRule = config.module.rules.find((rule) =>
+      rule?.["test"]?.test(".svg"),
+    );
+    if (imageRule) {
+      imageRule["exclude"] = /\.svg$/;
+    }
+
+    // Configure .svg files to be loaded with @svgr/webpack
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
     return config;
   },
 };
