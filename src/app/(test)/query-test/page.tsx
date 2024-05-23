1ddb5ee68a54a44eb5d17db354566487a2580db0
Pridesd <whwotjr98@naver.com>
2024-05-23 19:44:31 +0900

    chore: turn on lint-check build

--- src/app/(test)/query-test/page.tsx
+++ src/app/(test)/query-test/page.tsx
@@ -1,10 +1,10 @@
-import { CombineSuspenseDelays, SingleSuspenseDelays } from './Components'
+// import { CombineSuspenseDelays, SingleSuspenseDelays } from './Components'
 
 const QueryTestPage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SingleSuspenseDelays />
-      <CombineSuspenseDelays />
+      {/* <SingleSuspenseDelays />
+      <CombineSuspenseDelays /> */}
     </main>
   )
 }
