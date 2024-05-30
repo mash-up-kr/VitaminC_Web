7c12a8fa869cc8e2eb1421e81b15a90bc24bb77d
hee-suh <suhee0206@gmail.com>
2024-05-30 20:21:20 +0900

    fix: update cors origin

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': 'http://localhost:3000',
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 }
