3267a4a5390961debe70b6286cc896955e60a765
hee-suh <suhee0206@gmail.com>
2024-05-30 20:15:05 +0900

    fix: add corsHeaders to route handler to fix networkerror: fetch failed

---
+++ src/app/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': 'http://localhost:3000',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
+export function middleware(request: Request) {
+  if (request.method === 'OPTIONS') {
+    return NextResponse.json({}, { headers: corsHeaders })
+  }
+}
+
+export const config = {
+  matcher: '/api/:path*',
+}
