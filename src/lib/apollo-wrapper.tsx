a5c63e8edde40224f6ab47523d1d035b494e84ed
hee-suh <suhee0206@gmail.com>
2024-05-30 20:03:41 +0900

    fix: remove fetchOptions: { cache: 'no-store' }
    [ApolloError]: Dynamic server usage: no-store fetch http://localhost:3000/api/graphql /cc

--- src/lib/apollo-wrapper.tsx
+++ src/lib/apollo-wrapper.tsx
@@ -19,7 +19,7 @@ const makeClient = () => {
     uri: 'http://localhost:3000/api/graphql', // TODO: change url
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-    fetchOptions: { cache: 'no-store' },
+    // fetchOptions: { cache: 'no-store' },
     // you can override the default `fetchOptions` on a per query basis
     // via the `context` property on the options passed as a second argument
     // to an Apollo Client data fetching hook, e.g.:
