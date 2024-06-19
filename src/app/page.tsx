fb531d661bcac4b5ffe34fcbca29024cd14470f8
Lee <43488305+poiu694@users.noreply.github.com>
2024-06-19 12:57:20 +0900

    feat: text to black with testing PR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Home = () => {
   return (
     <main className="w-full min-h-screen flex flex-col justify-center items-center">
-      <ul className="list-disc text-xl space-y-4">
+      <ul className="list-disc text-xl text-black space-y-4">
         <li>
           <Link href="/cc" className="underline hover:text-sky-400">
             Client Components Example
