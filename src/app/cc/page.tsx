4b62a022e3a67439d94d9e87752a16e55a6ae2b7
hee-suh <suhee0206@gmail.com>
2024-06-23 15:59:09 +0900

    test: add bottom sheet to client component

--- src/app/cc/page.tsx
+++ src/app/cc/page.tsx
@@ -3,6 +3,7 @@
 import React, { Suspense, use } from 'react'
 
 import { fetchData } from '@/app/data'
+import BottomSheet from '@/components/bottom-sheet/index'
 import { Button } from '@/components/button'
 import LoadingIndicator from '@/components/loading-indicator'
 import { ResponseOk } from '@/models/interface'
@@ -25,6 +26,7 @@ const Page = () => {
         className="bg-sky-400"
         onClick={() => alert('ok!')}
       />
+      <BottomSheet header={<div>header</div>} body={<div>body content</div>} />
     </>
   )
 }
