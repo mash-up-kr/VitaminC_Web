73f7e2856f1a805f48cc6886064640c360388d9c
Pridesd <whwotjr98@naver.com>
2024-06-23 18:09:47 +0900

    feat: set kakao map type"

---
+++ src/types/map/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Core
+ */
+/// <reference path="Map.d.ts" />
+/// <reference path="LatLng.d.ts" />
+/// <reference path="event.d.ts" />
+/// <reference path="Marker.d.ts" />
+
+/**
+ * Miscellaneous
+ */
+/// <reference path="maps.d.ts" />
