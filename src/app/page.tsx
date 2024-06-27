755bfe6b7958ecb287d5f71f913b690a2f7f9fe6
Pridesd <whwotjr98@naver.com>
2024-06-27 13:45:45 +0900

    feat: set intro name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 
-const Home = () => {
+const Intro = () => {
   return (
     <main className="w-full min-h-screen flex flex-col justify-center items-center">
+      <p className="text-black">인트로 화면</p>
       <ul className="list-disc text-xl space-y-4">
         <li>
           <Link href="/cc" className="underline hover:text-sky-400">
@@ -19,4 +20,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Intro
