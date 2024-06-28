b88bd461135cd1166bcc7f0f17bdd4ee4b63e4fd
Lee <43488305+poiu694@users.noreply.github.com>
2024-06-28 21:50:08 +0900

    feat: button component with story

---
+++ src/components/common/button.tsx
@@ -0,0 +1,77 @@
+import type { ButtonHTMLAttributes } from 'react'
+import { cva } from 'class-variance-authority'
+
+import cn from '@/utils/cn'
+
+const ButtonVariants = cva<{
+  colorScheme: Record<'orange' | 'neutral', string>
+  padding: Record<'sm' | 'md' | 'lg', string>
+  width: Record<'sm' | 'md' | 'lg' | 'full' | 'fit', string>
+}>(
+  'flex justify-center items-center rounded-xl cursor-pointer text-neutral-000 text-[18px] font-semibold leading-normal',
+  {
+    variants: {
+      colorScheme: {
+        orange: 'bg-main-orange',
+        neutral: 'bg-neutral-500',
+      },
+      padding: {
+        sm: 'p-[12px]',
+        md: 'p-[15px]',
+        lg: 'p-[18px]',
+      },
+      width: {
+        sm: 'w-[126px]',
+        md: 'w-[197px]',
+        lg: 'w-[265px]',
+        fit: 'w-fit',
+        full: 'w-full flex-1',
+      },
+    },
+    defaultVariants: {
+      colorScheme: 'orange',
+      padding: 'md',
+      width: 'full',
+    },
+  },
+)
+
+const disabledClass = 'bg-neutral-600 text-neutral-400 cursor-not-allowed'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  colorScheme?: 'orange' | 'neutral'
+  padding?: 'sm' | 'md' | 'lg'
+  width?: 'sm' | 'md' | 'lg' | 'full' | 'fit'
+}
+
+function Button({
+  className,
+  children,
+  disabled,
+  padding,
+  width,
+  colorScheme,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      type="button"
+      disabled={disabled}
+      aria-disabled={disabled}
+      className={cn(
+        ButtonVariants({
+          colorScheme,
+          padding,
+          width,
+        }),
+        className,
+        disabled && disabledClass,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
---
+++ src/stories/button.stories.tsx
@@ -0,0 +1,62 @@
+// Button.stories.tsx
+
+import Button from '@/components/common/button'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    colorScheme: {
+      control: { type: 'radio' },
+      options: ['orange', 'neutral'],
+    },
+    padding: {
+      control: { type: 'select' },
+      options: ['sm', 'md', 'lg'],
+    },
+    width: {
+      control: { type: 'select' },
+      options: ['sm', 'md', 'lg', 'full', 'fit'],
+    },
+    disabled: {
+      control: { type: 'boolean' },
+    },
+    onClick: { action: 'clicked' },
+  },
+} as Meta
+
+type Story = StoryObj<typeof Button>
+
+export const Default: Story = {
+  args: {
+    colorScheme: 'neutral',
+    padding: 'md',
+    width: 'md',
+    disabled: false,
+    children: 'Default',
+    onClick: () => console.log('default'),
+  },
+}
+
+export const Orange: Story = {
+  args: {
+    colorScheme: 'orange',
+    padding: 'md',
+    width: 'md',
+    disabled: false,
+    children: 'Orange',
+    onClick: () => console.log('orange'),
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    colorScheme: 'neutral',
+    padding: 'md',
+    width: 'md',
+    disabled: true,
+    children: 'Disabled',
+    onClick: () => console.log('disabled'),
+  },
+}
