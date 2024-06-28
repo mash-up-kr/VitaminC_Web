a3c23628e7ae7bb3834c6af4a19337f63ab181e7
Lee <43488305+poiu694@users.noreply.github.com>
2024-06-28 22:23:20 +0900

    feat: confirm-cancel buttons component with story

---
+++ src/components/ConfirmCancelButton.tsx
@@ -0,0 +1,29 @@
+import cn from '@/utils/cn'
+import { Button } from './common'
+
+interface ConfirmCancelButtonProps {
+  className?: string
+  cancelLabel: string
+  confirmLabel: string
+  onCancel: VoidFunction
+  onConfirm: VoidFunction
+}
+
+export const ConfirmCancelButton = ({
+  className,
+  cancelLabel,
+  confirmLabel,
+  onCancel,
+  onConfirm,
+}: ConfirmCancelButtonProps) => {
+  return (
+    <div className={cn('flex gap-3', className)}>
+      <Button colorScheme="neutral" width="sm" onClick={onCancel}>
+        {cancelLabel}
+      </Button>
+      <Button colorScheme="orange" onClick={onConfirm}>
+        {confirmLabel}
+      </Button>
+    </div>
+  )
+}
---
+++ src/stories/ConfirmCancelButton.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ConfirmCancelButton } from '@/components/ConfirmCancelButton'
+
+const meta: Meta<typeof ConfirmCancelButton> = {
+  title: 'Components/ConfirmCancelButton',
+  component: ConfirmCancelButton,
+  argTypes: {
+    className: {
+      control: 'text',
+    },
+    cancelLabel: {
+      control: 'text',
+    },
+    confirmLabel: {
+      control: 'text',
+    },
+    onCancel: { action: 'cancel' },
+    onConfirm: { action: 'confirm' },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof ConfirmCancelButton>
+
+export const Default: Story = {
+  args: {
+    className: '',
+    cancelLabel: 'Cancel',
+    confirmLabel: 'Confirm',
+    onCancel: () => console.log('cancel'),
+    onConfirm: () => console.log('confirm'),
+  },
+}
