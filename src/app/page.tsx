2035090778b6c983cc16345968b129b287c4bdb7
Lee <43488305+poiu694@users.noreply.github.com>
2024-06-29 19:40:27 +0900

    chore: test qr code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { QRCode } from '@/components'
+
 const Home = () => {
   return (
-    <main className="w-full min-h-screen flex flex-col justify-center items-center">
-      HOme
+    <main className="w-full min-h-screen flex flex-col justify-center items-center bg-neutral-600">
+      <QRCode url="http://localhost:3000" />
     </main>
   )
 }
