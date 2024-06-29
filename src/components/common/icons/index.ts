2c349b42414e7d6b935bc9e2cdc85b57cf640aa2
Pridesd <whwotjr98@naver.com>
2024-06-29 20:47:23 +0900

    feat: set icon

--- src/components/common/icons/index.ts
+++ src/components/common/icons/index.ts
@@ -1,5 +1,35 @@
-import HeartStraight from './HeartStraight.svg'
+import CaretDown from './CaretDown.svg'
+import CaretUp from './CaretUp.svg'
+import Check from './Check.svg'
+import Clock from './Clock.svg'
+import Close from './Close.svg'
+import Delete from './Delete.svg'
+import HeartStraightFilled from './HeartStraightFilled.svg'
+import HeartStraightOutlined from './HeartStraightOutlined.svg'
+import Info from './Info.svg'
+import InfoCircle from './InfoCircle.svg'
+import Plus from './Plus.svg'
+import Search from './Search.svg'
+import ShareNetwork from './ShareNetwork.svg'
+import SignOut from './SignOut.svg'
+import ToastPopup from './ToastPopup.svg'
+import UploadSimple from './UploadSimple.svg'
 
 export const icons = {
-  heartStraight: HeartStraight,
+  caretDown: CaretDown,
+  caretUp: CaretUp,
+  check: Check,
+  clock: Clock,
+  close: Close,
+  delete: Delete,
+  heartStraightFilled: HeartStraightFilled,
+  heartStraightOutlined: HeartStraightOutlined,
+  info: Info,
+  infoCircle: InfoCircle,
+  plus: Plus,
+  search: Search,
+  shareNetwork: ShareNetwork,
+  signOut: SignOut,
+  toastPopup: ToastPopup,
+  uploadSimple: UploadSimple,
 }
