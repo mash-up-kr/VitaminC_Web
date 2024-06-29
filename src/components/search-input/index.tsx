5f82a7ec5429191d18623947ea4746432661059e
Pridesd <whwotjr98@naver.com>
2024-06-29 17:15:13 +0900

    feat: set design without Icon

--- src/components/search-input/index.tsx
+++ src/components/search-input/index.tsx
@@ -6,46 +6,69 @@ import { VariantProps, cva } from 'class-variance-authority'
 const SearchInputVariants = cva<{
   variant: Record<'outlined' | 'filled', string>
   size: Record<'sm' | 'md' | 'lg', string>
-}>(`leading-none caret-main-orange rounded-md placeholder:text-neutral-400`, {
-  variants: {
-    variant: {
-      outlined: 'border border-solid border-neutral-400 bg-neutral-800',
-      filled:
-        'bg-neutral-600 outline-none outline-offset-0 focus:outline-1 focus:outline-neutral-300',
+}>(
+  `w-full leading-none caret-main-orange rounded-md placeholder:text-neutral-400`,
+  {
+    variants: {
+      variant: {
+        outlined: 'border border-solid border-neutral-400 bg-neutral-800',
+        filled:
+          'bg-neutral-600 outline-none outline-offset-0 focus:outline-1 focus:outline-neutral-300',
+      },
+      size: {
+        sm: 'p-3 pr-[50px]',
+        md: 'p-4 pr-[50px]',
+        lg: 'p-5 pr-[50px]',
+      },
     },
-    size: {
-      sm: 'p-3',
-      md: 'p-4',
-      lg: 'p-5',
+    defaultVariants: {
+      variant: 'filled',
+      size: 'md',
     },
   },
-  defaultVariants: {
-    variant: 'filled',
-    size: 'md',
-  },
-})
+)
 
 interface SearchInputProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'>,
     VariantProps<typeof SearchInputVariants> {
+  iconType?: 'reset' | 'search'
   variant?: 'outlined' | 'filled'
   size?: 'sm' | 'md' | 'lg'
+  onReset?: () => void
 }
 
 const INITIAL_PLACEHOLDER = '원하는 장소를 검색해 주세요.'
 
 const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
   (
-    { variant, size, className, placeholder = INITIAL_PLACEHOLDER, ...props },
+    {
+      variant,
+      size,
+      className,
+      onReset,
+      iconType = 'reset',
+      placeholder = INITIAL_PLACEHOLDER,
+      ...props
+    },
     ref,
   ) => {
     return (
-      <input
-        {...props}
-        className={cn(SearchInputVariants({ variant, size }), className)}
-        ref={ref}
-        placeholder={placeholder}
-      />
+      <div className="w-full relative">
+        <input
+          {...props}
+          className={cn(SearchInputVariants({ variant, size }), className)}
+          ref={ref}
+          placeholder={placeholder}
+        />
+        {/* TODO: ClearIcon */}
+        {iconType === 'reset' && onReset && props.value ? (
+          <span className="absolute top=1/2 translate-y-1/2 right-4">X</span>
+        ) : null}
+        {/* TODO: SearchIcon */}
+        {iconType === 'search' ? (
+          <span className="absolute top=1/2 translate-y-1/2 right-4">?</span>
+        ) : null}
+      </div>
     )
   },
 )
