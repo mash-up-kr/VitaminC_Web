91745022da4b788d431937550e7968954fde35a7
Lee <43488305+poiu694@users.noreply.github.com>
2024-06-30 03:19:47 +0900

    feat: add colorscheme type in chip button

--- src/components/common/chip-button.tsx
+++ src/components/common/chip-button.tsx
@@ -5,21 +5,21 @@ import { cva, VariantProps } from 'class-variance-authority'
 import cn from '@/utils/cn'
 import Icon from './icon'
 import Typography from './typography'
+import type { ColorKey } from '@/types/color'
 
-const ChipButtonVariants = cva(
-  'rounded-3xl flex justify-center items-center px-[10px] py-2',
-  {
-    variants: {
-      colorScheme: {
-        neutral: 'bg-neutral-500',
-        orange: 'bg-orange-4000',
-      },
-    },
-    defaultVariants: {
-      colorScheme: 'neutral',
+const ChipButtonVariants = cva<{
+  colorScheme: Record<'neutral' | 'orange', `bg-${ColorKey}`>
+}>('rounded-3xl flex justify-center items-center px-[10px] py-2', {
+  variants: {
+    colorScheme: {
+      neutral: 'bg-neutral-500',
+      orange: 'bg-orange-400',
     },
   },
-)
+  defaultVariants: {
+    colorScheme: 'neutral',
+  },
+})
 
 interface ChipButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
