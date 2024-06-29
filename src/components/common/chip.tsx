a7c728af499ff1dbadf700d83b07fcbb20326044
Lee <43488305+poiu694@users.noreply.github.com>
2024-06-29 14:23:02 +0900

    refactor: extend size, colorScheme in chip

--- src/components/common/chip.tsx
+++ src/components/common/chip.tsx
@@ -5,25 +5,34 @@ import { cva } from 'class-variance-authority'
 import cn from '@/utils/cn'
 
 const ChipVariants = cva<{
-  colorScheme: Record<'neutral', string>
+  colorScheme: Record<'neutral' | 'orange', string>
+  size: Record<'sm' | 'md' | 'lg', string>
 }>('rounded px-2 py-1 text-[13px] font-medium leading-normal', {
   variants: {
     colorScheme: {
       neutral: 'bg-neutral-600 text-neutral-300',
+      orange: 'bg-neutral-800 text-main-orange',
+    },
+    size: {
+      sm: 'px-[6px] py-[2px]',
+      md: 'px-2 py-1',
+      lg: 'px-[10px] py-[6px]',
     },
   },
   defaultVariants: {
     colorScheme: 'neutral',
+    size: 'md',
   },
 })
 
 interface ChipProps extends HTMLAttributes<HTMLSpanElement> {
   colorScheme?: 'neutral'
+  size?: 'sm' | 'md' | 'lg'
   children: ReactNode
 }
 
 const Chip = forwardRef<HTMLSpanElement, ChipProps>(function Chip(
-  { className, children, colorScheme = 'neutral', ...props },
+  { className, children, colorScheme, size, ...props },
   ref,
 ) {
   return (
@@ -32,6 +41,7 @@ const Chip = forwardRef<HTMLSpanElement, ChipProps>(function Chip(
       className={cn(
         ChipVariants({
           colorScheme,
+          size,
         }),
         className,
       )}
