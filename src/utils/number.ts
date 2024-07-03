46c8791e10745a579bd71954403ec3cb11648a05
polee <poiu694@naver.com>
2024-07-03 13:59:10 +0900

    feat: roundToNthDecimal util function

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,5 +1,5 @@
-const sum = (a: number, b: number) => {
-  return a + b
+// 소수점 n 번째 자리에서 반올림
+export const roundToNthDecimal = (value: number, n: number): number => {
+  const factor = Math.pow(10, n - 1)
+  return Math.round(value * factor) / factor
 }
-
-export { sum }
