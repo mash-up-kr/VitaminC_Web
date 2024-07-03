41fe946e2a925b57fd81f90a756425fdedb1136c
polee <poiu694@naver.com>
2024-07-03 17:30:02 +0900

    feat: recent keywords local storage

--- src/utils/storage/index.ts
+++ src/utils/storage/index.ts
@@ -2,3 +2,9 @@ import { LocalStorageManager } from './local-storage'
 
 export const AUTH_KEY = '@@auth-token'
 export const authTokenStorage = new LocalStorageManager<string>(AUTH_KEY)
+
+export const RECENT_SEARCH_KEYWORD_KEY = '@@recent-search-keyword-key'
+export const recentSearchStorage = new LocalStorageManager<string[]>(
+  RECENT_SEARCH_KEYWORD_KEY,
+  [],
+)
