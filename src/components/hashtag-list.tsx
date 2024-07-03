c52fd8ecdc1dd6411893267927c0f88cdb69ae0d
Pridesd <whwotjr98@naver.com>
2024-07-03 17:20:30 +0900

    feat: set filtering logic

--- src/components/hashtag-list.tsx
+++ src/components/hashtag-list.tsx
@@ -1,24 +1,58 @@
+import { useEffect, useRef, useState } from 'react'
+
 import { Chip } from '@/components'
 import cn from '@/utils/cn'
 
 interface HashtagListProps {
+  placeId: string
   hashtags: string[]
   className?: string
 }
 
-const HashtagList = ({ hashtags, className }: HashtagListProps) => {
+const HashtagList = ({ placeId, hashtags, className }: HashtagListProps) => {
+  const [visibleHashtags, setVisibleHashtags] = useState(hashtags)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const editHashtag = () => {
+      if (!containerRef.current) return
+
+      const containerWidth = containerRef.current.offsetWidth
+      let totalWidth = 0
+      const newVisibleHashtags = hashtags.filter((tag, idx) => {
+        const chip = document.querySelector<HTMLElement>(
+          `#${placeId}-hashtag-${tag}`,
+        )
+
+        if (chip === null) return false
+
+        const chipWidth = chip.offsetWidth + 4
+        totalWidth += chipWidth
+
+        if (totalWidth < containerWidth) {
+          return true
+        }
+        return false
+      })
+      setVisibleHashtags(newVisibleHashtags)
+    }
+    editHashtag()
+  }, [hashtags])
+
   return (
     <div
+      ref={containerRef}
       className={cn(
         'w-full max-w-[380px] flex items-center gap-2 overflow-x-scroll no-scrollbar',
         className,
       )}
     >
-      {hashtags.map((hashtag) => (
+      {visibleHashtags.map((hashtag) => (
         <Chip
+          id={`${placeId}-hashtag-${hashtag}`}
           className="whitespace-nowrap"
           colorScheme="neutral-600"
-          key={`${hashtag}`}
+          key={`${placeId}-${hashtag}`}
         >{`#${hashtag}`}</Chip>
       ))}
     </div>
