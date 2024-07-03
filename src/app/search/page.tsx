baa72fc9176c08d8bd3f92096d90a479858e664a
polee <poiu694@naver.com>
2024-07-03 17:47:24 +0900

    feat: search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,16 +1,34 @@
 'use client'
 
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 import SearchInput from '@/components/search-input'
 import { ChipButton, Typography } from '@/components'
 import { recentSearchStorage } from '@/utils/storage'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 const Search = () => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const search = searchParams.get('search') ?? ''
+
   const [recentKeywords, setRecentKeywords] = useState(
     recentSearchStorage.getValueOrNull() ?? [],
   )
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState(search)
+  const isShowRecentKeywords =
+    query === '' && !!recentKeywords.length && !!search
+
+  const createQueryString = useCallback(
+    (key: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString())
+      params.set(key, value)
+
+      return params.toString()
+    },
+    [searchParams],
+  )
 
   const addUniqueKeyword = (keyword: string) => {
     const existRecentKeywords = [...(recentKeywords || [])]
@@ -23,8 +41,11 @@ const Search = () => {
   const searchByKeyword = async (formData: FormData) => {
     const searchKeyword = formData.get('query') as string | null
     if (searchKeyword) {
+      createQueryString('search', searchKeyword)
       addUniqueKeyword(searchKeyword)
       setQuery('')
+      // TODO: API 연동 및 결과 컴포넌트 로드
+      router.push(`${pathname}?${createQueryString('search', searchKeyword)}`)
     }
   }
 
@@ -39,18 +60,18 @@ const Search = () => {
   }
 
   return (
-    <div className="mt-2">
+    <div className="w-full min-h-dvh bg-neutral-700 px-5 pt-2">
       <form action={searchByKeyword}>
         <SearchInput
           name="query"
           value={query}
-          // TODO: router 연동
           leftIcon={{
             icon: {
               type: 'caretLeft',
               size: 'xl',
             },
             label: '뒤로 가기',
+            onClick: () => router.back(),
           }}
           rightIcon={{
             icon: { type: 'delete', size: 'xl', onClick: () => setQuery('') },
@@ -59,7 +80,8 @@ const Search = () => {
           onChange={(e) => setQuery(e.target.value)}
         />
       </form>
-      {recentKeywords.length && (
+
+      {isShowRecentKeywords && (
         <div className="mt-3 flex flex-col gap-3">
           <Typography size="h6" color="neutral-300">
             최근 검색어
