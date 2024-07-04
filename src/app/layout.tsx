88ee2c246fdb152ecdc1329128127975a4161a2f
Joo-Byungho <kjcoco13@gmail.com>
2024-07-04 11:33:07 +0900

    Feat: Add google measurement tools
    - Install next third parties dependencies
    - Include analytics and tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 import Script from 'next/script'
 
@@ -10,6 +11,9 @@ export const metadata: Metadata = {
   title: '꼬르륵',
   description: '전설의 보물섬으로 가는 맛집 지도',
 }
+
+const gaId = 'G-J03LXYNV9K'
+const gtagId = 'GTM-N8FPN3G2'
 
 export default function RootLayout({
   children,
@@ -31,6 +35,8 @@ export default function RootLayout({
           {children}
         </main>
       </body>
+      <GoogleAnalytics gaId={gaId} />
+      <GoogleTagManager gtmId={gtagId} />
     </html>
   )
 }
