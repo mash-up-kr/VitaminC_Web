e760fa27d706a4cbc3cb0a24edc2819af4154c97
Joo-Byungho <kjcoco13@gmail.com>
2024-07-04 14:10:08 +0900

    feat: add sentry (#39)
    * Feat: 센트리 설정 해줬어요
    
    * Feat: 디코도 연동했는데 잘 안됨 마무리 해줭
    
    * style: apply lint
    
    * feat: update fetch url
    
    ---------
    
    Co-authored-by: hee-suh <suhee0206@gmail.com>

---
+++ src/app/global-error.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import * as Sentry from '@sentry/nextjs'
+import NextError from 'next/error'
+import { useEffect } from 'react'
+
+const parseError = (error: Error): string => {
+  return `웹팀 채찍 맞아라~~ 🦹🏿‍♀️👹🦹🏿
+
+    에러 메세지 : ${error.message}
+
+    에러 위치 : ${error.stack
+      ?.split('\n')
+      .slice(0, 2)
+      .map((message) => message.trim())
+      .join('\n')}
+
+    에러 환경 : ${process.env.NODE_ENV}
+
+    당장 고쳐서 올렷!
+  `
+}
+
+const GlobalError = ({ error }: { error: Error & { digest?: string } }) => {
+  useEffect(() => {
+    Sentry.captureException(error)
+
+    const errorMessage = parseError(error)
+
+    fetch(`${window.location.origin}/api/error-alert`, {
+      method: 'POST',
+      body: JSON.stringify({ errorMessage }),
+    })
+  }, [error])
+
+  return (
+    <html>
+      <body>
+        {/* `NextError` is the default Next.js error page component. Its type
+        definition requires a `statusCode` prop. However, since the App Router
+        does not expose status codes for errors, we simply pass 0 to render a
+        generic error message. */}
+        <NextError statusCode={0} />
+      </body>
+    </html>
+  )
+}
+
+export default GlobalError
