0c3eb04a3bc0084146f0d272cfca0dc0ce786096
Pridesd <whwotjr98@naver.com>
2024-07-04 13:25:28 +0900

    chore: rename numOfReviews....

--- src/components/place/place-auto-search-item.tsx
+++ src/components/place/place-auto-search-item.tsx
@@ -3,63 +3,71 @@ import { Icon, Typography } from '@/components'
 import { PlaceProps } from './types'
 
 interface PlaceAutoSearchItemProps extends PlaceProps {
-  numOfreview: number
+  numOfReviews: number
   query: string
 }
 
 const PlaceAutoSearchItem = forwardRef<
   HTMLDivElement,
   PlaceAutoSearchItemProps
->(({ placeId, name, address, numOfreview, query, category, distance }, ref) => {
-  const nameParts = name.split(new RegExp(`(${query})`, 'gi'))
+>(
+  (
+    { placeId, name, address, numOfReviews, query, category, distance },
+    ref,
+  ) => {
+    const nameParts = name.split(new RegExp(`(${query})`, 'gi'))
 
-  return (
-    <section
-      ref={ref}
-      className="bg-neutral-700 p-5 pb-3 border-b-[1px] border-b-neutral-600 flex gap-2.5"
-    >
-      <Icon type="subtract" size="lg" />
-      <div className="flex gap-3.5">
-        <div className="flex flex-col gap-1.5 w-[230px]">
-          <div className="flex flex-col gap-[3px] ">
-            <Typography as="h2" size="h5">
-              {nameParts.map((text) => {
-                return text === query ? (
-                  <span key={`${placeId}-${text}`}>{text}</span>
-                ) : (
-                  <span key={`${placeId}-${text}`} className="text-neutral-300">
-                    {text}
-                  </span>
-                )
-              })}
-            </Typography>
-            <Typography size="body3" color="neutral-300">
-              {address}
-            </Typography>
+    return (
+      <section
+        ref={ref}
+        className="bg-neutral-700 p-5 pb-3 border-b-[1px] border-b-neutral-600 flex gap-2.5"
+      >
+        <Icon type="subtract" size="lg" />
+        <div className="flex gap-3.5">
+          <div className="flex flex-col gap-1.5 w-[230px]">
+            <div className="flex flex-col gap-[3px] ">
+              <Typography as="h2" size="h5">
+                {nameParts.map((text) => {
+                  return text === query ? (
+                    <span key={`${placeId}-${text}`}>{text}</span>
+                  ) : (
+                    <span
+                      key={`${placeId}-${text}`}
+                      className="text-neutral-300"
+                    >
+                      {text}
+                    </span>
+                  )
+                })}
+              </Typography>
+              <Typography size="body3" color="neutral-300">
+                {address}
+              </Typography>
+            </div>
+
+            <p className="flex items-center gap-1">
+              <Typography as="span" size="body4" color="neutral-500">
+                리뷰
+              </Typography>
+              <Typography as="span" size="body4" color="neutral-200">
+                {numOfReviews}
+              </Typography>
+            </p>
           </div>
 
-          <p className="flex items-center gap-1">
-            <Typography as="span" size="body4" color="neutral-500">
-              리뷰
+          <div className="flex flex-col gap-1.5">
+            <Typography as="span" size="body4" color="neutral-400">
+              {category}
             </Typography>
-            <Typography as="span" size="body4" color="neutral-200">
-              {numOfreview}
+            <Typography as="span" size="body4" color="neutral-400">
+              {distance}
             </Typography>
-          </p>
+          </div>
         </div>
-
-        <div className="flex flex-col gap-1.5">
-          <Typography as="span" size="body4" color="neutral-400">
-            {category}
-          </Typography>
-          <Typography as="span" size="body4" color="neutral-400">
-            {distance}
-          </Typography>
-        </div>
-      </div>
-    </section>
-  )
-})
+      </section>
+    )
+  },
+)
 
 PlaceAutoSearchItem.displayName = 'PlaceAutoSearchItem'
 
