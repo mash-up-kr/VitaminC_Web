951148055880e62ef3a159c4312298557c91df56
Lee <43488305+poiu694@users.noreply.github.com>
2024-07-04 23:39:15 +0900

    feat: map bound session storage

--- src/utils/storage/index.ts
+++ src/utils/storage/index.ts
@@ -1,4 +1,5 @@
 import { LocalStorageManager } from './local-storage'
+import { SessionStorageManager } from './session-storage'
 
 export const AUTH_KEY = '@@auth-token'
 export const authTokenStorage = new LocalStorageManager<string>(AUTH_KEY)
@@ -8,3 +9,11 @@ export const recentSearchStorage = new LocalStorageManager<string[]>(
   RECENT_SEARCH_KEYWORD_KEY,
   [],
 )
+
+export const MAP_BOUND = '@@map-bound'
+export const mapBoundSessionStorage = new SessionStorageManager<{
+  x1: number
+  y1: number
+  x2: number
+  y2: number
+}>(MAP_BOUND)
