8ee10b18d4ce8ebf83c4690c8d79b6c65857b356
polee <poiu694@naver.com>
2024-07-15 10:03:37 +0900

    feat: use-modal-transition for animation with render-state hook

---
+++ src/hooks/use-modal-transition.ts
@@ -0,0 +1,44 @@
+import type { CSSProperties } from 'react'
+import { useEffect, useState } from 'react'
+
+import useRenderState from './use-render-state'
+
+const useModalTransition = (
+  isOpen: boolean,
+  initial?: CSSProperties,
+  animate?: CSSProperties,
+  exit?: CSSProperties,
+  delay: string = '0.3s',
+) => {
+  const [state, transition] = useRenderState('unmounted')
+  const [styles, setStyles] = useState(initial)
+
+  useEffect(() => {
+    if (isOpen && state === 'unmounted') {
+      transition('mounting')
+      setStyles({ ...initial, transition: `all ${delay}` })
+
+      setTimeout(() => {
+        transition('mounted')
+        setStyles({
+          ...animate,
+          transition: `all ${delay}`,
+        })
+      }, 100)
+    } else if (!isOpen && state === 'mounted') {
+      transition('unmounting')
+      setStyles({
+        ...exit,
+        transition: `all ${delay}`,
+      })
+
+      setTimeout(() => {
+        transition('unmounted')
+      }, 300)
+    }
+  }, [isOpen, state, initial, animate, exit, transition, delay])
+
+  return { state, styles }
+}
+
+export default useModalTransition
