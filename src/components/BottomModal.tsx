8b621bce8db8742c608cba847c23e238682d2976
polee <43488305+poiu694@users.noreply.github.com>
2024-07-16 08:54:44 +0900

    feat: bottom modal with animation (#57)
    * refactor: change props type use-event-listener for event type
    
    * feat: use-click-outside with ref hook
    
    * feat: use-modal-transition for animation with render-state hook
    
    * feat: implement portal-component
    
    * feat: implement modal component with animation mount, unmount
    
    * feat: timing functions in modal animation transition
    
    * feat: bottom modal with animating slide
    
    * chore: change timing function unit to ms number
    
    * refactor: add as h3 tag for bottom modal layout headling

---
+++ src/components/BottomModal.tsx
@@ -0,0 +1,102 @@
+import type { ReactNode } from 'react'
+
+import cn from '@/utils/cn'
+import Modal from './common/Modal/Modal'
+import { Button, Typography } from './common'
+import type { ClassName } from '@/models/interface'
+
+interface BottomModalLayoutProps extends ClassName {
+  title: string
+  body?: string | ReactNode
+  cancelMessage: string
+  confirmMessage: string
+  onCancel: VoidFunction
+  onConfirm: VoidFunction
+}
+
+const BottomModalLayout = ({
+  title,
+  body,
+  className,
+  cancelMessage,
+  confirmMessage,
+  onCancel,
+  onConfirm,
+}: BottomModalLayoutProps) => {
+  return (
+    <div
+      className={cn(
+        'w-full top-auto bottom-0 translate-y-0 bg-neutral-700 rounded-t-[20px] px-5 pt-6 flex flex-col gap-3',
+        className,
+      )}
+    >
+      <Typography
+        as="h3"
+        size="h3"
+        color="neutral-000"
+        className="whitespace-pre-line"
+      >
+        {title}
+      </Typography>
+      {typeof body === 'string' ? (
+        <Typography
+          size="body2"
+          color="neutral-200"
+          className="mt-1 whitespace-pre-line"
+        >
+          {body}
+        </Typography>
+      ) : (
+        body
+      )}
+
+      <div className="w-full h-[94px] flex justify-center items-center gap-2">
+        <Button colorScheme="neutral" onClick={onCancel}>
+          {cancelMessage}
+        </Button>
+        <Button colorScheme="orange" onClick={onConfirm}>
+          {confirmMessage}
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+interface BottomModalProps extends BottomModalLayoutProps {
+  isOpen: Parameters<typeof Modal>[0]['isOpen']
+  onClose: Parameters<typeof Modal>[0]['onClose']
+}
+
+const BottomModal = ({
+  isOpen,
+  onClose,
+  title,
+  body,
+  confirmMessage,
+  cancelMessage,
+  onCancel,
+  onConfirm,
+}: BottomModalProps) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      initial={{ bottom: '-50%', opacity: 0.2 }}
+      animate={{ bottom: 0, opacity: 1 }}
+      exit={{ bottom: '-50%', opacity: 0.2 }}
+      delayTiming={500}
+      className="w-full max-w-[420px] top-auto bottom-0 translate-y-0"
+    >
+      <BottomModalLayout
+        title={title}
+        body={body}
+        cancelMessage={cancelMessage}
+        confirmMessage={confirmMessage}
+        onCancel={onCancel}
+        onConfirm={onConfirm}
+      />
+    </Modal>
+  )
+}
+
+export default BottomModal
---
+++ src/hooks/use-modal-transition.ts
@@ -0,0 +1,46 @@
+import type { CSSProperties } from 'react'
+import { useEffect, useState } from 'react'
+
+import useRenderState from './use-render-state'
+
+const DEFAULT_DELAY = 500
+
+const useModalTransition = (
+  isOpen: boolean,
+  initial?: CSSProperties,
+  animate?: CSSProperties,
+  exit?: CSSProperties,
+  delay: number = DEFAULT_DELAY,
+) => {
+  const [state, transition] = useRenderState('unmounted')
+  const [styles, setStyles] = useState(initial)
+
+  useEffect(() => {
+    if (isOpen && state === 'unmounted') {
+      transition('mounting')
+      setStyles({ ...initial, transition: `all ${delay}ms ease-in` })
+
+      setTimeout(() => {
+        transition('mounted')
+        setStyles({
+          ...animate,
+          transition: `all ${delay}ms ease-in`,
+        })
+      }, 0)
+    } else if (!isOpen && state === 'mounted') {
+      transition('unmounting')
+      setStyles({
+        ...exit,
+        transition: `all ${delay}ms ease-out`,
+      })
+
+      setTimeout(() => {
+        transition('unmounted')
+      }, 300)
+    }
+  }, [isOpen, state, initial, animate, exit, transition, delay])
+
+  return { state, styles }
+}
+
+export default useModalTransition
