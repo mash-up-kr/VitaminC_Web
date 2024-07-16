563dc12b2682811461bd43a742ee8f4f6425a2c0
Pridesd <whwotjr98@naver.com>
2024-07-16 16:33:01 +0900

    feat: set search page anchor box

---
+++ src/app/map/[mapId]/search-anchor-box.tsx
@@ -0,0 +1,18 @@
+import { Icon, Typography } from '@/components'
+import Link from 'next/link'
+
+const SearchAnchorBox = ({ mapId }: { mapId: string }) => {
+  return (
+    <Link
+      className="py-3.5 px-4 flex items-center justify-between bg-neutral-800 rounded-md border border-solid border-neutral-400"
+      href={`/search?mapId=${mapId}`}
+    >
+      <Typography size="body1" color="neutral-400">
+        원하는 장소를 검색해보세요
+      </Typography>
+      <Icon type="search" size="lg" stroke="neutral-400" />
+    </Link>
+  )
+}
+
+export default SearchAnchorBox
