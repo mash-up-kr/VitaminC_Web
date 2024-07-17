99223366bf5951cd6e5c7a8c2b6c17b1d057b062
Pridesd <whwotjr98@naver.com>
2024-07-17 14:02:26 +0900

    feat: set PlaceType

--- src/components/place/types.ts
+++ src/components/place/types.ts
@@ -12,3 +12,40 @@ export interface PlaceProps {
     onClickLike: () => void
   }
 }
+
+export interface PlaceType {
+  place: {
+    id: number
+    kakaoPlace: {
+      id: number
+      name: string
+      category: string
+      address: string
+      x: number
+      y: number
+      menuList: {
+        menu: string
+        price: string
+      }[]
+      photoList: string[]
+    }
+    x: number
+    y: number
+  }
+  tags: [
+    {
+      id: number
+      mapId: string
+      content: string
+      createdAt: string
+    },
+  ]
+  comments: [{}]
+  likedUserIds: number[]
+  createdBy: {
+    id: number
+    nickname: string
+  }
+  createdAt: string
+  updatedAt: string
+}
