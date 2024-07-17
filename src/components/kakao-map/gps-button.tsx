f7e9158267bb059b240732a82b74bda9a45d2751
Pridesd <whwotjr98@naver.com>
2024-07-17 13:44:24 +0900

    feat: set gps marker

--- src/components/kakao-map/gps-button.tsx
+++ src/components/kakao-map/gps-button.tsx
@@ -3,17 +3,35 @@ import type { RefObject } from 'react'
 import { AccessibleIconButton } from '@/components'
 import { ClassName } from '@/models/interface'
 import useWindowSize from '@/hooks/use-window-size'
+import { useKakaoMap } from './context'
+import GpsMarker from './gps-marker'
+import useUserGeoLocation from '@/hooks/use-user-geo-location'
 
 interface GpsButtonProps extends ClassName {
-  gpsMode: boolean
   bottomRef?: RefObject<HTMLDivElement>
-  onClickGps: VoidFunction
 }
 
 const GpsButton = forwardRef<HTMLButtonElement, GpsButtonProps>(
-  ({ gpsMode, bottomRef, onClickGps }, ref) => {
+  ({ bottomRef }, ref) => {
+    const userLocation = useUserGeoLocation()
     const [gpsBottomPosition, setGpsBottomPosition] = useState(16)
+    const [gpsMode, setGpsMode] = useState(false)
     const { height } = useWindowSize()
+    const { map } = useKakaoMap()
+
+    const handleGpsClick = () => {
+      if (!map) return
+
+      if (!gpsMode) {
+        const location = new window.kakao.maps.LatLng(
+          userLocation.latitude,
+          userLocation.longitude,
+        )
+        map.setCenter(location)
+      }
+
+      setGpsMode((prev) => !prev)
+    }
 
     useEffect(() => {
       const getGpsButtonPositionY = () => {
@@ -31,19 +49,27 @@ const GpsButton = forwardRef<HTMLButtonElement, GpsButtonProps>(
     }, [bottomRef?.current, height])
 
     return (
-      <AccessibleIconButton
-        ref={ref}
-        className={`absolute right-5 transition-all z-10`}
-        style={{
-          bottom: `${gpsBottomPosition}px`,
-        }}
-        label={gpsMode ? '내 위치로 이동 취소' : '내 위치로 이동'}
-        icon={{
-          className: 'w-11 h-11',
-          type: gpsMode ? 'locationOn' : 'locationOff',
-        }}
-        onClick={onClickGps}
-      />
+      <>
+        {gpsMode && (
+          <GpsMarker
+            latitude={userLocation.latitude}
+            longitude={userLocation.longitude}
+          />
+        )}
+        <AccessibleIconButton
+          ref={ref}
+          className={`absolute right-5 transition-all z-10`}
+          style={{
+            bottom: `${gpsBottomPosition}px`,
+          }}
+          label={gpsMode ? '내 위치로 이동 취소' : '내 위치로 이동'}
+          icon={{
+            className: 'w-11 h-11',
+            type: gpsMode ? 'locationOn' : 'locationOff',
+          }}
+          onClick={handleGpsClick}
+        />
+      </>
     )
   },
 )
