e030ad4844baea5f427f88dacdbc0c8480ed52d2
Pridesd <whwotjr98@naver.com>
2024-07-19 19:23:16 +0900

    chore: set map type

--- src/components/boarding-pass/boarding-members.tsx
+++ src/components/boarding-pass/boarding-members.tsx
@@ -13,11 +13,11 @@ const BoardingMembers = ({ owner, members }: BoardingMembersProps) => {
   return (
     <ul className="w-full bg-neutral-600 max-h-[268px] overflow-y-scroll no-scrollbar">
       {members.map((member, index) => (
-        <li key={member} className="flex items-center px-4 h-[52px]">
+        <li key={member.id} className="flex items-center px-4 h-[52px]">
           <Avatar
-            // TODO: 로직 수정
-            me={member === owner}
-            value={member}
+            // TODO: 사용자 정보 cookie에 저장하는 로직 완성 후 적용
+            me={member.id === 3}
+            value={member.nickname}
             colorScheme={memberColors[index % memberColors.length]}
           />
           <Typography
@@ -26,9 +26,9 @@ const BoardingMembers = ({ owner, members }: BoardingMembersProps) => {
             color="neutral-100"
             className="ml-2"
           >
-            {member}
+            {member.nickname}
           </Typography>
-          {owner === member && (
+          {owner.id === member.id && (
             <Chip size="sm" colorScheme="neutral-800" className="ml-[6px]">
               주인장
             </Chip>
