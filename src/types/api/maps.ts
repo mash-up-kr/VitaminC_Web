2f7008f72e7eed63122d8a5df17cbf8a85c564a4
Lee <43488305+poiu694@users.noreply.github.com>
2024-07-21 22:30:42 +0900

    refactor: tag to optional

--- src/types/api/maps.ts
+++ src/types/api/maps.ts
@@ -2,7 +2,7 @@ import type { IconKey } from '@/components/common/icon'
 
 export interface TagItem {
   id: number
-  type: IconKey
+  type?: IconKey
   mapId: string
   content: string
   createdAt: string
