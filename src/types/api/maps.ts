921e6c07ecf04d8e3db689f703bd9bb03b6e0776
Pridesd <whwotjr98@naver.com>
2024-07-22 09:41:12 +0900

    feat: remove MapDataType

--- src/types/api/maps.ts
+++ src/types/api/maps.ts
@@ -4,18 +4,3 @@ export interface TagItem {
   content: string
   createdAt: string
 }
-
-export interface MapDataType {
-  id: string
-  name: string
-  users: MapMemberData[]
-  registeredPlaceCount: number
-  createdAt: Date
-  updatedAt: Date
-}
-
-export interface MapMemberData {
-  id: number
-  nickname: string
-  role: string
-}
