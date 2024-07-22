38a710e5c31e5c262e770d6eca1a5b08f9831cf6
polee <poiu694@naver.com>
2024-07-22 17:18:55 +0900

    chore: un-used params

--- src/app/place/[placeId]/page.tsx
+++ src/app/place/[placeId]/page.tsx
@@ -1,11 +1,7 @@
 import { PLACE_DETAIL_DATA } from '@/constants/place'
 import PlaceBox from './place-box'
 
-const PlaceDetail = async ({
-  searchParams,
-}: {
-  searchParams?: { mapId?: string; search?: string }
-}) => {
+const PlaceDetail = async () => {
   // TODO: place/id api 연결
   const place = { data: PLACE_DETAIL_DATA }
 
