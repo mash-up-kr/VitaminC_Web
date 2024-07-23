9bd77e049397b3327a0718ee514e3ea9b6481e6b
hee-suh <suhee0206@gmail.com>
2024-07-23 21:12:40 +0900

    feat: add clamp() util to limit range

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -3,3 +3,8 @@ export const roundToNthDecimal = (value: number, n: number): number => {
   const factor = Math.pow(10, n - 1)
   return Math.round((value + Number.EPSILON) * factor) / factor
 }
+
+// value를 [min, max]로 제한
+export const clamp = (value: number, min: number, max: number): number => {
+  return Math.min(Math.max(value, min), max)
+}
