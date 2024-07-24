29455b8974642a1ccd7cec4dd85a7b2a2081245f
hee-suh <suhee0206@gmail.com>
2024-07-24 20:19:02 +0900

    feat: add image to intro new-map step

--- src/components/intro/steps/new-map.tsx
+++ src/components/intro/steps/new-map.tsx
@@ -1,5 +1,6 @@
 import { Button, Typography } from '@/components/common'
 import { IntroActionDispatch } from '@/app/intro/page'
+import IntroNewMapImage from '/public/intro-making-map.png'
 
 const NewMap = ({ goNextStep }: IntroActionDispatch) => {
   const handleClick = () => {
@@ -9,22 +10,18 @@ const NewMap = ({ goNextStep }: IntroActionDispatch) => {
   return (
     <>
       <div className="flex-1">
-        <div className="pt-12 px-5 mb-12">
+        <div className="pt-12 px-5 mb-[54px]">
           <Typography
             size="h1"
             color="neutral-000"
-            className="whitespace-pre-line mb-4"
+            className="whitespace-pre-line"
           >
             {`새 지도 만들고\n항해를 시작할까요?`}
           </Typography>
         </div>
 
         <div className="w-full flex justify-center items-center">
-          <img
-            className="h-[220px] object-fill"
-            src="/chunsik.png"
-            alt="미정"
-          />
+          <img src={IntroNewMapImage.src} width="100%" alt="보물지도" />
         </div>
       </div>
 
