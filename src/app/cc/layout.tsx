b5c9b11c512deca11fbce1dbe8a9400f821473e9
Pridesd <whwotjr98@naver.com>
2024-07-25 21:44:34 +0900

    chore: remove cc rsc page

--- src/app/cc/layout.tsx
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children }: React.PropsWithChildren) => {
-  return (
-    <div className="w-full min-h-screen flex flex-col justify-center items-center gap-4">
-      {children}
-    </div>
-  )
-}
-
-export default Layout
