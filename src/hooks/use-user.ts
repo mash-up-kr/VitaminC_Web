eb5a7a99babb1b9cb35eb5921b072343c1d4d12a
polee <poiu694@naver.com>
2024-07-30 15:13:10 +0900

    feat: use-user hook for user data

---
+++ src/hooks/use-user.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+
+import type { User } from '@/models/user.interface'
+import { api } from '@/utils/api'
+import { APIError } from '@/models/interface'
+
+const useUser = () => {
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setLoading(true)
+      try {
+        const { data } = await api.users.me.get()
+
+        setUser(data)
+      } catch (err) {
+        if (err instanceof APIError) setError(err.message)
+        else setError('예상치 못한 오류가 발생했습니다.')
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchUser()
+  }, [])
+
+  return { user, loading, error }
+}
+
+export default useUser
