4095dd0486b9b6330161d84c0d6bf2005edbf64c
hee-suh <suhee0206@gmail.com>
2024-08-05 13:13:51 +0900

    feat: update css of skeleton

--- src/app/search/[query]/result-place-popup-skeleton.tsx
+++ src/app/search/[query]/result-place-popup-skeleton.tsx
@@ -3,22 +3,24 @@ import { forwardRef } from 'react'
 const ResultPlacePopupSkeleton = forwardRef<HTMLDivElement>(({}, ref) => {
   return (
     <div
-      className="w-full h-[175px] rounded-[10px] bg-neutral-700 p-5 flex flex-col gap-4 z-10 animate-pulse"
+      className="w-full h-[165px] rounded-[10px] bg-neutral-700 p-5 z-10"
       ref={ref}
     >
-      <div className="flex flex-col justify-between w-full">
-        <div className="flex flex-col gap-1">
-          <div className="w-[90px] h-[36px] bg-[#353538] rounded-3xl" />
+      <div className="flex flex-col gap-3 animate-pulse">
+        <div className="flex justify-between w-full">
+          <div className="flex flex-col gap-3">
+            <div className="w-[90px] h-[24px] bg-[#353538] rounded-md" />
 
-          <div className="w-[160px] h-[26px] bg-[#353538] rounded-3xl" />
+            <div className="w-[180px] h-[20px] bg-[#353538] rounded-md" />
 
-          <div className="mt-2 w-[240px] h-[20px] bg-[#353538] rounded-3xl" />
+            <div className="w-[160px] h-[20px] bg-[#353538] rounded-md" />
+          </div>
 
-          <div className="mt-2 w-[180px] h-[20px] bg-[#353538] rounded-3xl" />
+          <div className="w-20 h-20 bg-[#353538] rounded-md" />
         </div>
-      </div>
 
-      <div className="w-20 h-20" />
+        <div className="w-full h-[20px] bg-[#353538] rounded-md" />
+      </div>
     </div>
   )
 })
