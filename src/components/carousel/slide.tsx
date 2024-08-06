e31575740a271206a7e0a39ebd3afc36f6fdaf17
hee-suh <suhee0206@gmail.com>
2024-08-06 16:11:19 +0900

    feat: prevent horizontal wheel scroll in carousel

--- src/components/carousel/slide.tsx
+++ src/components/carousel/slide.tsx
@@ -4,7 +4,8 @@ import { type CSSProperties, useCallback, useEffect, useRef } from 'react'
 import { type PanInfo, animate, motion, useMotionValue } from 'framer-motion'
 import cn from '@/utils/cn'
 import type { Item } from './types'
-import { Typography } from '../common'
+import { Typography } from '@/components/common'
+import useEventListener from '@/hooks/use-event-listener'
 
 interface SlideProps {
   items: Item[]
@@ -35,7 +36,7 @@ const getObjectFitClass = (objectFit: CSSProperties['objectFit']): string => {
 const Slide = ({
   items,
   activeIndex,
-  objectFit = 'contain',
+  objectFit = 'cover',
   handleChangeActiveIndex,
 }: SlideProps) => {
   const x = useMotionValue(0)
@@ -72,6 +73,18 @@ const Slide = ({
     const controls = animate(x, calculateNewX(), transition)
     return controls.stop
   }, [activeIndex, calculateNewX, x])
+
+  // 마우스휠(트랙패드 제스처 포함)로 수평 스크롤하지 못하도록 방지
+  // ∵ framer motion에서 해당 이벤트를 감지하는 제스처가 없어서, activeIndex 업데이트를 하지 못합니다.
+  useEventListener({
+    type: 'wheel',
+    listener: (event) => {
+      if (event.deltaX) {
+        event.preventDefault()
+      }
+    },
+    options: { passive: false },
+  })
 
   return (
     <motion.div
