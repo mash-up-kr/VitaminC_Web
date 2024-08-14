b9cd82b26c328b2ce1d4c4b2d26928ee2cdfbfd8
hee-suh <suhee0206@gmail.com>
2024-08-14 23:37:20 +0900

    feat: add status & rename loading 2 fetching in useFetch

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -7,7 +7,7 @@ import { revalidate as revalidateRoute } from '@/utils/api/route'
 import { getTimeDiff } from '@/utils/date'
 
 const cache: Record<string, { data: any; timestamp: Date }> = {}
-const loadingMap: Record<string, boolean> = {}
+const fetching: Record<string, boolean> = {}
 
 const CACHE_TIME = 5 * 60 * 1000 // 5분
 
@@ -25,6 +25,8 @@ const useFetch = <T>(
 
   const cacheKey = options?.key?.join('')
   const apiKey = options?.key?.join('') ?? queryFn?.toString() ?? ''
+
+  const disabled = typeof options?.enabled === 'boolean' && !options.enabled
 
   const revalidate = useCallback((key: string[] | string) => {
     const targetKey = typeof key === 'string' ? key : key.join('')
@@ -45,26 +47,16 @@ const useFetch = <T>(
     }
 
     const fetchData = async () => {
-      if (typeof options?.enabled === 'boolean' && !options.enabled) return
-      if (cacheKey && cache[cacheKey]?.data) {
-        const ms = getTimeDiff(cache[cacheKey].timestamp, new Date())
-        const isExpired = ms >= CACHE_TIME
-        if (!isExpired) {
-          setData(cache[cacheKey].data)
-          handleLoadEnd(cache[cacheKey].data)
-          return
-        }
-        revalidate(cacheKey)
-      }
-
       if (!queryFn) return
 
       try {
-        loadingMap[apiKey] = true
+        fetching[apiKey] = true
+
         const response = await queryFn()
 
         setData(response.data)
         handleLoadEnd(response.data)
+
         if (cacheKey) {
           cache[cacheKey] = { data: response.data, timestamp: new Date() }
         }
@@ -73,6 +65,7 @@ const useFetch = <T>(
           if (err.status === 401) {
             revalidateRoute('token')
             deleteCookie('Authorization')
+            window.location.reload()
           }
           setError({
             name: 'API Error',
@@ -87,15 +80,40 @@ const useFetch = <T>(
           })
         }
       } finally {
-        loadingMap[apiKey] = false
+        fetching[apiKey] = false
       }
     }
 
-    fetchData()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [apiKey, cacheKey, options?.enabled])
+    const getData = async () => {
+      if (disabled) return
 
-  return { data, loading: loadingMap[apiKey], error, revalidate, clear }
+      if (cacheKey && cache[cacheKey]?.data) {
+        const timeDiff = getTimeDiff(cache[cacheKey].timestamp, new Date())
+        const isExpired = timeDiff >= CACHE_TIME
+        if (!isExpired) {
+          setData(cache[cacheKey].data)
+          handleLoadEnd(cache[cacheKey].data)
+          return
+        }
+        revalidate(cacheKey)
+      }
+
+      fetchData()
+    }
+
+    getData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [apiKey, cacheKey, disabled])
+
+  return {
+    data,
+    loading: fetching[apiKey], // fetching으로 대체 예정
+    fetching: fetching[apiKey] ?? false,
+    error,
+    status: data ? 'success' : error ? 'error' : disabled ? 'idle' : 'pending',
+    revalidate,
+    clear,
+  }
 }
 
 export default useFetch
