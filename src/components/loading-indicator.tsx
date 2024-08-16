bbdb77714f098d1734326f43b898220c5d53c688
hee-suh <suhee0206@gmail.com>
2024-08-17 00:41:23 +0900

    fix: prevent ssr issue in lottie-react using dynamic import
    cf. ReferenceError: document is not defined

--- src/components/loading-indicator.tsx
+++ src/components/loading-indicator.tsx
@@ -1,4 +1,4 @@
-import Lottie from 'lottie-react'
+import { Lottie } from '@/components/common/lottie'
 import LoadingLottie from '../../public/lotties/loading.json'
 
 const LoadingIndicator = () => {
@@ -9,6 +9,7 @@ const LoadingIndicator = () => {
     >
       <img src="/images/treasure.png" width="100%" alt="보물지도" />
       <Lottie animationData={LoadingLottie} className="w-1/3 h-[47px]" />
+      {/* <Lottie path={'/lotties/loading.json'} className="w-1/3 h-[47px]" /> */}
     </div>
   )
 }
