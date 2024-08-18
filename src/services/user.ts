e23e2313bd8ce08c9e1e4886df3b557e1c791d52
hee-suh <suhee0206@gmail.com>
2024-08-18 16:33:37 +0900

    fix: fix infinite reloading in production ㅜㅜ

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,11 +1,12 @@
-import { revalidate as revalidateRoute } from '@/utils/api/route'
+import { fetchData, revalidate as revalidateRoute } from '@/utils/api/route'
 import { notify } from '@/components/common/custom-toast'
 
 export const handleSignout = async () => {
   try {
-    await fetch('/api/token', { method: 'DELETE' })
-    revalidateRoute('token')
-    window.location.replace('/intro')
+    await fetchData('/api/token', { method: 'DELETE' }).then(() => {
+      revalidateRoute('token')
+      window.location.replace('/intro')
+    })
   } catch (error) {
     notify.error('로그아웃에 실패했습니다. ')
   }
