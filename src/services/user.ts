86fd6caa7a67b6e4d8ef63ba26c6d6a37f0926c6
hee-suh <suhee0206@gmail.com>
2024-08-18 20:49:43 +0900

    fix: fix infinite navigation in vercel production b/c cookie deletion is not working in vercel"

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,12 +1,12 @@
-import { fetchData, revalidate as revalidateRoute } from '@/utils/api/route'
+import { revalidate as revalidateRoute } from '@/utils/api/route'
 import { notify } from '@/components/common/custom-toast'
 
 export const handleSignout = async () => {
   try {
-    await fetchData('/api/token', { method: 'DELETE' }).then(() => {
-      revalidateRoute('token')
+    revalidateRoute('token')
+    if (window.location.pathname !== '/intro') {
       window.location.replace('/intro')
-    })
+    }
   } catch (error) {
     notify.error('로그아웃에 실패했습니다. ')
   }
