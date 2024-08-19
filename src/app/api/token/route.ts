6791bdb297458aa22b67fbfe1abb1c92c784e1db
Hee Suh <53266682+hee-suh@users.noreply.github.com>
2024-08-19 10:33:04 +0900

    fix: set cookie options exactly the same as server (#103)
    * fix: add httpOnly, secure option to cookies().set
    
    * fix: set cookie options exactly the same as server

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -33,8 +33,12 @@ export async function DELETE() {
   }
 
   cookieStore.set(AUTHORIZATION, '', {
-    maxAge: -1,
+    httpOnly: true,
+    sameSite: 'none',
+    secure: true,
+    path: '/',
     expires: new Date(Date.now() - 1),
+    domain: process.env.NODE_ENV === 'production' ? '.korrk.kr' : 'localhost',
   })
 
   return NextResponse.json({ message: 'success', data: {} })
