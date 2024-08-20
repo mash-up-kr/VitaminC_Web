cd2c02966d2b53e3c1c7006dca79e1e8e41e0f2a
polee <poiu694@naver.com>
2024-08-20 10:47:33 +0900

    feat: add og information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,16 @@ import { pretendard } from '@/styles/fonts'
 export const metadata: Metadata = {
   title: '꼬르륵',
   description: '전설의 보물섬으로 가는 맛집 지도',
+  keywords: ['꼬르륵', '맛집', '함께 만드는 맛집 지도'],
+  metadataBase: new URL('https://www.korrk.kr'),
+  openGraph: {
+    type: 'website',
+    title: '꼬르륵',
+    description: '보물섬으로 가는 맛집지도, 같이 채워볼래?',
+    locale: 'ko_KR',
+    images: 'https://www.korrk.kr/images/opengraph-image.png',
+    url: 'https://www.korrk.kr',
+  },
 }
 
 export default function RootLayout({
