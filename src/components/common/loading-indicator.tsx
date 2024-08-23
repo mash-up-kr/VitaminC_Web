5f80ba2e48505107dc76a673b1c65ab9752bae14
hee-suh <suhee0206@gmail.com>
2024-08-24 03:03:13 +0900

    feat: update image assets

--- src/components/common/loading-indicator.tsx
+++ src/components/common/loading-indicator.tsx
@@ -5,9 +5,14 @@ const LoadingIndicator = () => {
   return (
     <div
       role="status"
-      className="flex h-dvh w-full flex-col items-center justify-center bg-neutral-700"
+      className="flex h-dvh w-full flex-col items-center justify-center gap-[6px] bg-neutral-700"
     >
-      <img src="/images/treasure.png" width="100%" alt="보물지도" />
+      <img
+        src="/images/loading.png"
+        width="64px"
+        height="64px"
+        alt="보물지도"
+      />
       <Lottie animationData={LoadingLottie} className="h-[47px] w-1/3" />
     </div>
   )
