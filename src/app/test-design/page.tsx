09f3e47acaf0c6b5af388a8d9c19988a39f7da26
polee <poiu694@naver.com>
2024-09-06 17:46:50 +0900

    chore: remove test-modal

--- src/app/test-design/page.tsx
+++ src/app/test-design/page.tsx
@@ -2,45 +2,20 @@
 
 import { useState } from 'react'
 
-import InvitingBoardingPass from '@/components/boarding-pass/inviting-boarding-pass'
 import Input from '@/components/common/input'
-import Modal from '@/components/common/modal'
 
 const TestDesign = () => {
   const [input, setInput] = useState('')
-  const [isOpenModal, setIsOpenModal] = useState(false)
 
   return (
     <>
       <div className="flex h-dvh w-dvw flex-col gap-[40px]">
         <Input value={input} onChange={(value) => setInput(value)} />
 
-        <button type="button" onClick={() => setIsOpenModal(true)}>
-          초대장 디자인
-        </button>
-
         <a href="/test-design/map" className="text-3xl">
           MAP
         </a>
       </div>
-
-      <Modal
-        isOpen={isOpenModal}
-        onClose={() => setIsOpenModal(false)}
-        dimClassName="z-[9998]"
-        className="z-[9999] w-full max-w-[420px] px-5"
-      >
-        <InvitingBoardingPass
-          inviteCode={'test'}
-          expirationTime={new Date()}
-          mapName={'TEST'}
-          numOfCrews={3000}
-          creator={{
-            id: 1,
-            nickname: '상민이',
-          }}
-        />
-      </Modal>
     </>
   )
 }
