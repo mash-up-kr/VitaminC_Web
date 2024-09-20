d9db21e23c1cd4e714c029779b066d74be3203af
polee <poiu694@naver.com>
2024-09-20 11:10:11 +0900

    chore: test

--- src/app/test-design/page.tsx
+++ src/app/test-design/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import BoardingDivider from '@/components/boarding-pass/boarding-divider'
 import Typography from '@/components/common/typography'
 
 const TestDesign = () => {
@@ -31,6 +32,72 @@ const TestDesign = () => {
           800
         </Typography>
         <Typography size="body0" color="neutral-000" className="font-[900]">
+          900
+        </Typography>
+
+        <BoardingDivider className="h-[50px] w-full bg-neutral-500" />
+
+        <Typography
+          size="body0"
+          color="neutral-000"
+          className="font-serif font-[100]"
+        >
+          100
+        </Typography>
+        <Typography
+          size="body0"
+          color="neutral-000"
+          className="font-serif font-[200]"
+        >
+          200
+        </Typography>
+        <Typography
+          size="body0"
+          color="neutral-000"
+          className="font-serif font-[300]"
+        >
+          300
+        </Typography>
+        <Typography
+          size="body0"
+          color="neutral-000"
+          className="font-serif font-[400]"
+        >
+          400
+        </Typography>
+        <Typography
+          size="body0"
+          color="neutral-000"
+          className="font-serif font-[500]"
+        >
+          500
+        </Typography>
+        <Typography
+          size="body0"
+          color="neutral-000"
+          className="font-serif font-[600]"
+        >
+          weight-600
+        </Typography>
+        <Typography
+          size="body0"
+          color="neutral-000"
+          className="font-serif font-[700]"
+        >
+          weight-700
+        </Typography>
+        <Typography
+          size="body0"
+          color="neutral-000"
+          className="font-serif font-[800]"
+        >
+          800
+        </Typography>
+        <Typography
+          size="body0"
+          color="neutral-000"
+          className="font-serif font-[900]"
+        >
           900
         </Typography>
       </div>
