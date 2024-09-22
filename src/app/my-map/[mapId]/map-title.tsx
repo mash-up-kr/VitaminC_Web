cecafc95a281208474b058f684f04b0a2b0748a7
polee <43488305+poiu694@users.noreply.github.com>
2024-09-22 20:46:19 +0900

    feat: edit visibility of map (#137)
    * feat: map reveal edit
    
    * chore: rename function

--- src/app/my-map/[mapId]/map-title.tsx
+++ src/app/my-map/[mapId]/map-title.tsx
@@ -30,6 +30,7 @@ const MapTitle = ({
 }: MapTitleProps) => {
   const [mapNameInput, setMapNameInput] = useState(mapInfo.name || '')
   const [isOpenEditModal, setIsOpenEditModal] = useState(false)
+  const [isOpenPrivateModal, setIsOpenMapToPublicModal] = useState(false)
   const { revalidate } = useFetch()
 
   const handleChangeMapName = async () => {
@@ -46,26 +47,88 @@ const MapTitle = ({
       revalidate(['map', mapInfo.id])
       notify.success(`${mapNameInput}으로 바꾸었습니다.`)
       refetchMapInfo()
-      setIsOpenEditModal(false)
     } catch (err) {
       notify.error('서버에 문제가 생겼습니다.')
+    } finally {
+      setIsOpenEditModal(false)
     }
+  }
+
+  const updateMapVisibility = async (isPublic: boolean) => {
+    try {
+      const { id, description, name } = mapInfo
+      await api.maps.id.patch({
+        id,
+        isPublic,
+        description,
+        name,
+      })
+      revalidate(['map', mapInfo.id])
+      notify.success(`${mapInfo.name} 지도를 ${isPublic ? '공개' : '비공개'}로 바꾸었습니다.`)
+      refetchMapInfo()
+    } catch (err) {
+      notify.error('서버에 문제가 생겼습니다.')
+    } finally {
+      setIsOpenMapToPublicModal(false)
+    }
+  }
+
+  const handleClickMapReveal = () => {
+    if (!mapInfo.isPublic) {
+      // 비공개 => 공개
+      setIsOpenMapToPublicModal(true)
+      return
+    }
+
+    // 공개 => 비공개
+    updateMapVisibility(false)
   }
 
   return (
     <>
       <div className={cn('', className)}>
         {mapInfo.createBy.id === user.id ? (
-          <button
-            type="button"
-            className="flex items-center gap-1"
-            onClick={() => setIsOpenEditModal(true)}
-          >
-            <Typography size="body0-2" color="neutral-000">
-              {mapInfo.name}
-            </Typography>
-            <Icon type="pencil" size="xl" />
-          </button>
+          <div className="flex w-full items-center justify-between">
+            <button
+              type="button"
+              className="flex items-center gap-1"
+              onClick={() => setIsOpenEditModal(true)}
+            >
+              <Typography size="body0-2" color="neutral-000">
+                {mapInfo.name}
+              </Typography>
+              <Icon type="pencil" size="xl" />
+            </button>
+
+            <div className="w-fit rounded-[8px] bg-neutral-600 px-[3px] py-[4px]">
+              <button
+                type="button"
+                className="flex rounded-[8px]"
+                onClick={handleClickMapReveal}
+              >
+                <Typography
+                  size="h6"
+                  color={mapInfo.isPublic ? 'neutral-300' : 'neutral-000'}
+                  className={cn(
+                    'rounded-[8px] px-[6px] py-[3px]',
+                    !mapInfo.isPublic && 'bg-neutral-500',
+                  )}
+                >
+                  비공개
+                </Typography>
+                <Typography
+                  size="h6"
+                  color={mapInfo.isPublic ? 'neutral-000' : 'neutral-300'}
+                  className={cn(
+                    'rounded-[8px] px-[6px] py-[3px]',
+                    mapInfo.isPublic && 'bg-orange-400',
+                  )}
+                >
+                  공개
+                </Typography>
+              </button>
+            </div>
+          </div>
         ) : (
           <Typography size="body0-2" color="neutral-000">
             {mapInfo.name}
@@ -92,6 +155,20 @@ const MapTitle = ({
         onCancel={() => setIsOpenEditModal(false)}
         onConfirm={handleChangeMapName}
         disabled={!mapNameInput || mapNameInput === mapInfo.name}
+      />
+
+      <BottomModal
+        layout="confirm"
+        title={`${mapInfo.name}지도를 공개할까요?`}
+        isOpen={isOpenPrivateModal}
+        cancelMessage="취소"
+        confirmMessage="공개로 전환"
+        body={
+          '모두가 지도에 참여할 수 있고, 등록된 맛집을 확인하고 좋아요를 누를 수 있어요.'
+        }
+        onClose={() => setIsOpenMapToPublicModal(false)}
+        onCancel={() => setIsOpenMapToPublicModal(false)}
+        onConfirm={() => updateMapVisibility(true)}
       />
     </>
   )
