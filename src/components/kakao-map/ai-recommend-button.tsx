a0ae90cfd5f018f358cd0054031a6365bfd97ca4
Pridesd <whwotjr98@naver.com>
2024-09-22 23:44:16 +0900

    feat: 완성 ㅋ

--- src/components/kakao-map/ai-recommend-button.tsx
+++ src/components/kakao-map/ai-recommend-button.tsx
@@ -1,19 +1,33 @@
+import { useState } from 'react'
 import Icon from '../common/icon'
 import Typography from '../common/typography'
+import GptIntroModal from './gpt-intro-modal'
 
 const AiRecommendButton = () => {
+  const [isOpenGptIntro, setIsOpenGptIntro] = useState(false)
   return (
-    <button className="relative flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-br from-[#E5684C] to-[#5D5FEF] p-[1px] transition-all duration-300 ease-in-out hover:w-40">
-      <div className="flex h-full w-full items-center gap-2 overflow-hidden rounded-full bg-neutral-800 p-2.5">
-        <Icon type="gradientLogo" size="lg" className="shrink-0" />
-        <Typography
-          size="h5"
-          className="text-nowrap bg-gradient-to-r from-[#FBD5CD] to-[#CDCDFA] bg-clip-text text-transparent"
-        >
-          AI 맛집 추천 받기
-        </Typography>
-      </div>
-    </button>
+    <>
+      <button
+        className="relative flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-br from-[#E5684C] to-[#5D5FEF] p-[1px] transition-all duration-300 ease-in-out hover:w-40"
+        onClick={() => setIsOpenGptIntro(true)}
+      >
+        <div className="flex h-full w-full items-center gap-2 overflow-hidden rounded-full bg-neutral-800 p-2.5">
+          <Icon type="gradientLogo" size="lg" className="shrink-0" />
+          <Typography
+            size="h5"
+            className="text-nowrap bg-gradient-to-r from-[#FBD5CD] to-[#CDCDFA] bg-clip-text text-transparent"
+          >
+            AI 맛집 추천 받기
+          </Typography>
+        </div>
+      </button>
+      {isOpenGptIntro && (
+        <GptIntroModal
+          isOpen={isOpenGptIntro}
+          onClose={() => setIsOpenGptIntro(false)}
+        />
+      )}
+    </>
   )
 }
 
