a6fac6a2092b889d0e178ad5cf03cf52cd0843f1
Pridesd <whwotjr98@naver.com>
2024-09-23 17:55:52 +0900

    chore: rename bottomPositionY

--- src/components/kakao-map/floating-button-box.tsx
+++ src/components/kakao-map/floating-button-box.tsx
@@ -12,27 +12,26 @@ interface FloatingButtonBoxProps {
 
 const FloatingButtonBox = forwardRef<HTMLDivElement, FloatingButtonBoxProps>(
   ({ topOfBottomBounds }) => {
-    const [gpsBottomPositionY, setGpsBottomPositionY] =
-      useState(BUTTON_OFFSET_Y)
+    const [bottomPositionY, setBottomPositionY] = useState(BUTTON_OFFSET_Y)
     const { height: windowHeight } = useWindowSize()
 
     useEffect(() => {
       if (topOfBottomBounds) {
-        setGpsBottomPositionY(
+        setBottomPositionY(
           Math.min(
             windowHeight - topOfBottomBounds + BUTTON_OFFSET_Y,
             (windowHeight * 3) / 4 - BUTTON_OFFSET_Y - BUTTON_HEIGHT / 2,
           ),
         )
       } else {
-        setGpsBottomPositionY(BUTTON_OFFSET_Y)
+        setBottomPositionY(BUTTON_OFFSET_Y)
       }
     }, [topOfBottomBounds, windowHeight])
     return (
       <div
         className={`absolute right-5 z-10 flex flex-col items-end gap-2.5 transition-all duration-300 ease-in-out`}
         style={{
-          bottom: `${gpsBottomPositionY}px`,
+          bottom: `${bottomPositionY}px`,
         }}
       >
         <GpsButton />
