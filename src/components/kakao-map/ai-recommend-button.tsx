c0efa0c34b0eee761c6c07b5b7efbdaed11c5027
Pridesd <whwotjr98@naver.com>
2024-09-23 18:06:13 +0900

    style: icon size

--- src/components/kakao-map/ai-recommend-button.tsx
+++ src/components/kakao-map/ai-recommend-button.tsx
@@ -11,8 +11,8 @@ const AIRecommendButton = () => {
         className="relative flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-br from-[#E5684C] to-[#5D5FEF] p-[1px] transition-all duration-300 ease-in-out hover:w-40"
         onClick={() => setIsOpenGptIntro(true)}
       >
-        <div className="flex h-full w-full items-center gap-2 overflow-hidden rounded-full bg-neutral-800 p-2.5">
-          <Icon type="gradientLogo" size="lg" className="shrink-0" />
+        <div className="flex h-full w-full items-center gap-2 overflow-hidden rounded-full bg-neutral-800 p-2.5 pl-1.5">
+          <Icon type="gradientLogo" size="xl" className="shrink-0" />
           <Typography
             size="h5"
             className="text-nowrap bg-gradient-to-r from-[#FBD5CD] to-[#CDCDFA] bg-clip-text text-transparent"
