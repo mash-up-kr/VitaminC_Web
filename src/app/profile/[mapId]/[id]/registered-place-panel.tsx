72768d7ffdcbf07584a7cb3fcdee7936e4d3cf9b
Pridesd <whwotjr98@naver.com>
2024-10-03 14:14:38 +0900

    feat: 더보기를 추가했어용

--- src/app/profile/[mapId]/[id]/registered-place-panel.tsx
+++ src/app/profile/[mapId]/[id]/registered-place-panel.tsx
@@ -1,7 +1,6 @@
 import type { PlaceType } from '@/models/api/place'
 import type { MapInfo } from '@/models/map'
 import type { User } from '@/models/user'
-import { getMapId } from '@/services/map-id'
 import { api } from '@/utils/api'
 import { useEffect, useState } from 'react'
 import PlaceItem from './place-item'
@@ -9,6 +8,8 @@ import { APIError } from '@/models/api'
 import { notify } from '@/components/common/custom-toast'
 import EmptyPlaceList from '@/components/place/empty-place-list'
 import PlacePopupSkeleton from '@/components/place/place-popup-skeleton'
+import { INITIAL_VISIBLE_PLACE_LENGTH } from './liked-place-panel'
+import ChipButton from '@/components/common/chip-button'
 
 const RegisterededPlacePanel = ({
   userId,
@@ -17,7 +18,8 @@ const RegisterededPlacePanel = ({
   userId: User['id']
   mapId: MapInfo['id']
 }) => {
-  const [places, setPlaces] = useState<PlaceType[]>([])
+  const [places, setPlaces] = useState<PlaceType[]>()
+  const [showMorePlace, setShowMorePlace] = useState(false)
 
   const renderPlaces = () => {
     if (typeof places === 'undefined') {
@@ -39,18 +41,48 @@ const RegisterededPlacePanel = ({
     }
 
     return (
-      <div className="flex flex-col gap-2.5 py-[18px]">
-        {places.map((place) => (
-          <PlaceItem
-            key={place.place.id}
-            className="w-full border-[1px] border-neutral-500 bg-neutral-600"
-            mapId={mapId}
-            selectedPlace={place}
-          />
-        ))}
-      </div>
+      <>
+        <div className="flex flex-col gap-2.5 py-[18px]">
+          {places
+            .slice(
+              0,
+              showMorePlace ? places.length : INITIAL_VISIBLE_PLACE_LENGTH,
+            )
+            .map((place) => (
+              <PlaceItem
+                key={place.place.id}
+                className="w-full border-[1px] border-neutral-500 bg-neutral-600"
+                mapId={mapId}
+                selectedPlace={place}
+              />
+            ))}
+        </div>
+        {!showMorePlace && places.length > INITIAL_VISIBLE_PLACE_LENGTH && (
+          <div className="flex h-[71px] w-full items-center justify-center">
+            <ChipButton
+              fontSize="body3"
+              rightIcon={{
+                type: 'caretDown',
+                size: 'sm',
+                stroke: 'neutral-000',
+              }}
+              onClick={() => {
+                setShowMorePlace(true)
+              }}
+            >
+              더보기
+            </ChipButton>
+          </div>
+        )}
+      </>
     )
   }
+
+  useEffect(() => {
+    if (places) {
+      setShowMorePlace(places.length <= INITIAL_VISIBLE_PLACE_LENGTH)
+    }
+  }, [places])
 
   useEffect(() => {
     const getRegisteredPlace = async () => {
